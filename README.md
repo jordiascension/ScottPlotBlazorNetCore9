ce7d70cd90ff2ecb0c2034b19c227ee375e7039b
ENTERPRISEJORDI\jordi <jordiascension@enterpriseconsulting.es>
2025-08-25 13:02:34 +0200

    Added Scott Plot Demo with Blazor an .Net Core 9.0

---
+++ README.md
@@ -0,0 +1,16 @@
+\# Scott Plot Demo with Blazor and .Net Core 9.0
+
+Compile The project an open these urls:
+
+https://localhost:7202/counter
+
+![alt text](image-1.png)
+
+
+https://localhost:7202/fans
+
+![alt text](image.png)
+
+
+
+
---
+++ ScottPlotVersion9.Client/PlotTheme.cs
@@ -0,0 +1,32 @@
+﻿using ScottPlot;
+
+namespace ScottPlotVersion9.Client
+{
+    public static class PlotTheme
+    {
+        public static void ApplyDarkTheme(Plot plot)
+        {
+            // Fondo negro
+            plot.FigureBackground.Color = Colors.Black;
+            plot.DataBackground.Color = Colors.Black;
+
+            // Ejes y etiquetas en color blanco
+            foreach (var axis in plot.Axes.GetAxes())
+            {
+                axis.Label.ForeColor = Colors.White;
+            }
+
+            // Desactivar la cuadrícula predeterminada
+            plot.Grid.Equals(false);
+
+            // Añadir una cuadrícula personalizada
+            var customGrid = plot.Grid;
+            customGrid.MajorLineColor = Colors.White;
+            customGrid.MinorLineColor = Colors.Transparent;
+            customGrid.MajorLineWidth = 1;
+            customGrid.MajorLinePattern = LinePattern.Solid;
+
+            // Establecer el espaciado de la cuadrícula
+        }
+    }
+}
