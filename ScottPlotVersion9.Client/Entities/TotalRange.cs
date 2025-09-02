14665f910fa850ee2be96e0e87487c693ff12c3e
Alfonfdez <alfonfdez2@hotmail.com>
2025-09-02 12:38:47 +0200

    Fixed window size and added TotalRange enum

---
+++ ScottPlotVersion9.Client/Entities/TotalRange.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace ScottPlotVersion9.Client.Entities
+{
+    public enum TotalRange
+    {
+        [Description("MIN_1")]
+        Minute1,
+
+        [Description("MIN_5")]
+        Minute5,
+
+        [Description("MIN_30")]
+        Minute30,
+
+        [Description("HOUR_1")]
+        Hour1,
+
+        [Description("HOUR_4")]
+        Hour4,
+
+        [Description("HOUR_8")]
+        Hour8,
+
+        [Description("DAY_1")]
+        Day1,
+
+        [Description("DAY_7")]
+        Day7,
+
+        [Description("MONTH_1")]
+        Month1,
+    }
+}
---
+++ ScottPlotVersion9.Client/Utils/PlotEnumHelper.cs
@@ -0,0 +1,35 @@
+﻿using ScottPlotVersion9.Client.Entities;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace ScottPlotVersion9.Client.Utils
+{
+    public static class PlotEnumHelper
+    {
+        /// <summary>
+        /// Gets descriptions from any enum type.
+        /// </summary>
+        public static IEnumerable<string> GetEnumDescriptions<TEnum>() where TEnum : Enum
+        {
+            foreach (TEnum value in Enum.GetValues(typeof(TEnum)))
+            {
+                yield return GetEnumDescription(value as Enum);
+            }
+        }
+
+        private static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return (attributes.Length > 0) ? attributes[0].Description : value.ToString();
+        }
+
+        /// <summary>
+        /// Gets the Total Ranges list: "MIN_1", "MIN_5", "MIN_30", "HOUR_1", "HOUR_4", "HOUR_8", "DAY_1", "DAY_7", "MONTH_1".
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<string> GetTotalRanges() => GetEnumDescriptions<TotalRange>();
+    }
+}
